467df4c93375f8fe1c849e9e3df21dbc26f1c127
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-04-19 16:11:52 +0700

    Initial

--- crawlData.py
+++ crawlData.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from google.cloud import language_v1
+import six
+
+article_content = []
 
 
 def get_article(link: str):
@@ -7,19 +11,30 @@ def get_article(link: str):
     # print(content.text)
     soup = BeautifulSoup(content.content, 'html.parser')
     # get article content (<p> tag)
-    article_content = soup.find_all("div", "detail__content")
-    print(article_content)
+    found_content = soup.find_all("div", class_="detail__content", )
+    # print(soup.get_text())
+    return str(found_content)
 
 
 if __name__ == '__main__':
-    baseUrl = 'https://vneconomy.vn/'
-    links = []
-    company = input()
-    url = 'https://search.hemera.com.vn/search/1/' + company + '/time/1'
-    response = requests.get(url)
-    # Get returned json object
-    data = response.json()
-    for i in range(0, 10):
-        links.append(baseUrl + data['List'][i]['UrlArticle'] + '.htm')
-        print(links[i])
-    get_article(links[0])
+    check = True
+    while check:
+        baseUrl = 'https://vneconomy.vn/'
+        links = []
+        print("Enter company's name: ")
+        company = input()
+        url = 'https://search.hemera.com.vn/search/1/' + company + '/time/1'
+        response = requests.get(url)
+        # Get returned json object
+        data = response.json()
+        for i in range(0, 10):
+            links.append(baseUrl + data['List'][i]['UrlArticle'] + '.htm')
+            print(links[i])
+            # Output all article's content to an array
+            article_content.append(get_article(links[i]))
+            print(article_content[i])
+
+        print('Continue?')
+        check = input()
+        if check == 'n':
+            break
